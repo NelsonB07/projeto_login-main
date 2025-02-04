448c925b32e5632d67dfffebbf9f379e7d9bfe8d
Nelson Brandão Carvalho <jbrandao.72@gmail.com>
2025-02-03 23:20:45 -0300

    Primeira parte do JS concluída, maniupulação com DOM

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const signUpLink = document.querySelector('.login-form-subscribe a'); 
+    const loginLink = document.querySelector('.sign-up-form-subscribe a'); 
+
+    const showFormSignUp = () => {
+        document.querySelector(".login-form").style.display = "none";
+        document.querySelector(".sign-up-form").style.display = "flex";
+        document.querySelector(".login-text-content h2").innerHTML = 
+        "Olá!<br> Seja bem-vindo(a).";
+        document.querySelector(".login-text-content p").innerHTML = 
+        "Crie sua conta para acessar a plataforma.";
+    };
+
+    const showFormLogin = () => {
+        document.querySelector(".login-form").style.display = "flex";
+        document.querySelector(".sign-up-form").style.display = "none";
+        document.querySelector(".login-text-content h2").style.innerHTML = 
+        "Olá!<br> Seja bem-vindo(a) de volta.";
+        document.querySelector(".login-text-content p").style.innerHTML = 
+        "Faça login para continuar acessando essa página.";
+    }; 
+
+    signUpLink.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        showFormSignUp();
+    });
+
+    loginLink.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        showFormLogin();
+    });
+});
